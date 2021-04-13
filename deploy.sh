59077bf83a0e89bedb3b9f8bfe21337f6f5cff74
agitsafikri <agitsafikri@gmail.com>
2021-04-14 03:38:24 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git pull https://devsquad-id@github.com/devsquad-id/wabot-api-2021.git
+    git pull origin master
 # Exit maintenance mode
 php artisan up
 
