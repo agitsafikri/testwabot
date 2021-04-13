63fe1e67d9bfb4623084f4f19de3c77dd03ba88e
Agitsa Nur Fikri <agitsa.nurfikri@upi.edu>
2021-04-14 04:46:11 +0700

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,8 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git pull origin master
+    git pull origin master -Wabotapi2021
+    
 # Exit maintenance mode
 php artisan up
 
